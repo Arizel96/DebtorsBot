64233085c52545b0dd5e0a0a9ac1e21fa49af3ef
test <sanek96sm@gmail.com>
2017-01-19 22:11:07 +0200

    Just class with 2 attributes - name and credit.

---
+++ src/main/java/objects/Debtor.java
@@ -0,0 +1,31 @@
+package objects;
+
+/**
+ * Created by Arizel on 19.01.2017.
+ */
+public class Debtor {
+    private String name;
+    private long credit;
+
+    public Debtor(String name, long credit) {
+        this.name = name;
+        this.credit = credit;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCredit(long credit) {
+        this.credit = credit;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public long getCredit() {
+        return credit;
+    }
+}
